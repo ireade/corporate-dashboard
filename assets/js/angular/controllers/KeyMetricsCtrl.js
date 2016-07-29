2e1de4ece2c2f6b74d28935ea97b2519713c1c89
Ire Aderinokun <ire@ireaderinokun.com>
2016-07-29 09:25:49 +0100

    charts everywheregit status

--- assets/js/angular/controllers/KeyMetricsCtrl.js
+++ assets/js/angular/controllers/KeyMetricsCtrl.js
@@ -1,7 +1,111 @@
-app.controller('KeyMetricsCtrl', ['Customers', function(Customers) {
+app.controller('KeyMetricsCtrl', ['Customers', 'Issues', function(Customers, Issues) {
 
 
-	console.log(Customers);
+
+
+
+	/* Customers by Week Chart */
+
+	var weeks = [];
+	var numberOfCustomers = [];
+
+	angular.forEach(Customers, function(value) {
+		weeks.push(value.week);
+		numberOfCustomers.push(value.customers);
+	})
+
+	var customersChartData = {
+		datasets: [{
+	        data: numberOfCustomers,
+	        label: 'Customers'
+	    }],
+	    labels: weeks
+	};
+
+	var customersChartCtx = $("#customers");
+
+	var customersChart = new Chart(customersChartCtx, {
+		data: customersChartData,
+	    type: 'line',
+	    options: {
+	        elements: {
+	            arc: {
+	                borderColor: "#000000"
+	            }
+	        },
+	        legend: {
+	        	position: 'bottom'
+	        }
+	    }
+	});
+
+
+
+	/* Issues Chart */
+
+	console.log(Issues);
+
+	var totalIssues = 0;
+	var openIssues = 0;
+	var closedIssues = 0;
+
+
+
+
+	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+
+	var stuff = [0, 0, 0, 0, 0, 0, 0]
+
+	angular.forEach(Issues, function(value) {
+		totalIssues ++;
+		switch(value.status) {
+			case 'closed':
+				closedIssues++;
+				break;
+			case 'open':
+				openIssues++;
+				break;
+		}
+
+
+		var month = parseInt(value.submission_timestamp.split('-')[1]) - 1;
+
+		console.log(month);
+
+		stuff[month]++;
+
+	})
+
+
+	this.openIssues = openIssues;
+	this.totalIssues = totalIssues;
+
+	console.log(stuff);
+
+
+
+	
+
+	
+
+	var issuesChartData = {
+		datasets: [{
+	        data: stuff,
+	        label: 'Issues'
+	    }],
+	    labels: months
+	};
+	var issuesChartCtx = $("#issues");
+
+	var issuesChart = new Chart(issuesChartCtx, {
+		data: issuesChartData,
+	    type: 'bar',
+	    options: {
+	        legend: {
+	        	position: 'bottom'
+	        }
+	    }
+	});
 
 
 }]);
--- assets/js/angular/filters.js
+++ assets/js/angular/filters.js
@@ -12,32 +12,22 @@ app.filter('nameForURL', function() {
 app.filter('moment', function() {
 	return function(string) {
 
-
-
 		var rawDate = string;
 		var year = parseInt(rawDate.split('-')[0]);
 		var month = parseInt(rawDate.split('-')[1]) - 1;
 		var day = parseInt(rawDate.split('-')[2]);
 
-		// var rawTime = string.split('T')[1];
-		// var hours = parseInt(rawTime.split(':')[0]);
-		// var minutes = parseInt(rawTime.split(':')[1]);
-		// var seconds = parseInt(rawTime.split(':')[2]);
-
 		var m = moment().year(year).month(month).date(day);
-
-		//var m = moment().year(year).month(month).date(day).hours(hours).minutes(minutes);
 
 		m = m.calendar(null, {
 			sameDay: '[Today]',
 			nextDay: '[Tomorrow]',
 			lastDay: '[Yesterday]',
-			lastWeek: 'D MMMM, YYYY',
-			sameElse: 'D MMMM, YYYY'
+			lastWeek: 'D MMM, YYYY',
+			sameElse: 'D MMM, YYYY'
 		});
 	  
 		return m;
-
 
 	};
 });
